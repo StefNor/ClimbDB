8d200bf91bcfe3180d197eeafc4e3a9bdc7c5248
Stefan Norman <57892951+StefNor@users.noreply.github.com>
2025-01-20 10:54:24 +0100

    World -> GBG

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Hello, Climbers in the world!</title>
+    <title>PHP - Hello, Climbers in GBG!</title>
 </head>
 <body>
-        <h1><?php echo 'Climbers in the world!'; ?></h1>
+        <h1><?php echo 'Climbers in GBG!'; ?></h1>
 </body>
 </html>
