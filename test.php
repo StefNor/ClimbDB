8c003ffba6f855b3cc1266c1198321783e304901
Stefan Norman <57892951+StefNor@users.noreply.github.com>
2025-01-20 13:00:04 +0100

    update test.php

--- test.php
+++ test.php
@@ -1,2 +1,15 @@
 <?php
-	echo 'PHP Syntax';
+
+require 'config.php';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+
+try {
+	$pdo = new PDO($dsn, $user, $password);
+
+	if ($pdo) {
+		echo "Connected to the $db database successfully!";
+	}
+} catch (PDOException $e) {
+	echo $e->getMessage();
+}
